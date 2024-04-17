e3d4acac07e1c0aee5420787c3aa21b53a20f247
rednek21 <egor.podustov@mail.ru>
2024-04-17 15:42:45 +0300

    Themes crud

--- themes/urls.py
+++ themes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ThemeListAPIView, ThemeCreateAPIView
+from .views import ThemeListAPIView, ThemeCreateAPIView, ThemeRetrieveUpdateAPIView, ThemeDestroyAPIView, ThemeRetrieveAPIView
 
 app_name = 'themes'
 
 urlpatterns = [
-    path('themes/', ThemeListAPIView.as_view(), name='themes'),
+    path('themes/', ThemeListAPIView.as_view(), name='themes_list'),
     path('themes/create', ThemeCreateAPIView.as_view(), name='themes_create'),
+    path('themes/<int:pk>', ThemeRetrieveAPIView.as_view(), name='themes_retrieve'),
+    path('themes/<int:pk>/update', ThemeRetrieveUpdateAPIView.as_view(), name='themes_update'),
+    path('themes/<int:pk>/destroy', ThemeDestroyAPIView.as_view(), name='themes'),
 ]
--- themes/views.py
+++ themes/views.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
-from rest_framework.generics import ListAPIView, CreateAPIView
-
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Theme
@@ -28,4 +27,31 @@ class ThemeCreateAPIView(CreateAPIView):
     serializer_class = ThemeSerializer
     permission_classes = [
         permissions.AllowAny # После реализации работы с пользователем заменить на самописный permission для редакторов
+    ]
+
+
+class ThemeRetrieveAPIView(RetrieveAPIView):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+
+class ThemeRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+
+class ThemeDestroyAPIView(DestroyAPIView):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+    permission_classes = [
+        permissions.AllowAny
     ]
