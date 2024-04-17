a135194ad6948161748c340808b9897ce87b7176
rednek21 <egor.podustov@mail.ru>
2024-04-17 16:18:45 +0300

    throttling added

--- config/settings.py
+++ config/settings.py
@@ -174,14 +174,27 @@ AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
 
+    # AUTHENTICATION
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
+    # FILTERING
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    ],
+
+    # THROTTLING
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
+
 }
 
 DJOSER = {
