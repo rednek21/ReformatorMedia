a0a4ffe955dbe02e139712f0e5ed962bfe2bddbd
rednek21 <egor.podustov@mail.ru>
2024-04-19 14:24:09 +0300

    videos crud

--- videos/urls.py
+++ videos/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from videos.views import VideoListAPIView, VideoCreateAPIView
+from videos.views import VideoListAPIView, VideoCreateAPIView, ThemeRetrieveAPIView, RetrieveUpdateAPIView, ThemeDestroyAPIView
 
 app_name = 'videos'
 
 urlpatterns = [
     path('videos/', VideoListAPIView.as_view(), name='videos'),
     path('videos/create', VideoCreateAPIView.as_view(), name='videos_create'),
+    path('videos/<int:pk>/', ThemeRetrieveAPIView.as_view(), name='videos_retrieve'),
+    path('videos/<int:pk>/update', RetrieveUpdateAPIView.as_view(), name='videos_update'),
+    path('videos/<int:pk>/destroy', ThemeDestroyAPIView.as_view(), name='videos_destroy'),
 ]
