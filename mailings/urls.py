20f1fe7161c0f93565b3a7d89c26af8146da4c6f
rednek21 <egor.podustov@mail.ru>
2024-04-27 22:04:04 +0300

    create activation mailing object

---
+++ mailings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# from .views import
+
+app_name = 'mailings'
+
+urlpatterns = [
+    # path('mailings/', VideoListAPIView.as_view(), name='videos_list'),
+    # path('mailings/<int:pk>/update', ThemeRetrieveUpdateAPIView.as_view(), name='videos_update'),
+]
--- mailings/views.py
+++ mailings/views.py
@@ -1,23 +1,36 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from djoser import email
-from rest_framework.generics import CreateAPIView
+
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions
 
 from mailings.models import UserActivationMailing
 from mailings.serializers import UserActivationMailingSerializer
+
+from django.utils import timezone
+
+from users.models import User
 
 
 class UserActivationEmail(email.ActivationEmail):
     template_name = 'mailings/user_activation.html'
 
     def send(self, to, *args, **kwargs):
-        super().send(to, *args, **kwargs)
-        UserActivationMailing.objects.create(user=to)
+        try:
+            user = User.objects.get(email=str(to[0]))
+            UserActivationMailing.objects.create(user=user, created_at=timezone.now(), verified=False)
+            super().send(to, *args, **kwargs)
+        except User.DoesNotExist:
+            return HttpResponseNotFound("User not found")
 
 
-# class UserActivationMailingCreateAPIView(CreateAPIView):
-#     queryset = UserActivationMailing.objects.all()
-#     serializer_class = UserActivationMailingSerializer
-#     permission_classes = [
+# class UserActivationMailingVerifyAPIView(APIView):
+#     # permission_classes = [
+#     #     permissions.
+#     # ]
 #
-#     ]
+#     authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
